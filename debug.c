7094e48eddc8f3822e7e69db2d6847612594df0e
septicalls <septicalls@proton.me>
2023-09-01 23:41:37 +0530

    disassembled constant instruction

--- debug.c
+++ debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 void disassembleChunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
@@ -15,15 +16,25 @@ static int simpleInstruction(const char *name, int offset) {
     return offset + 1;
 }
 
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
+    uint8_t constant = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant);
+    printValue(chunk->constants.values[constant]);
+    printf("'\n");
+    return offset + 2;
+}
+
 int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
-    case OP_RETURN:
-        return simpleInstruction("OP_RETURN", offset);
-    default:
-        printf("Unknown opcode %d\n", instruction);
-        return offset + 1;
+        case OP_CONSTANT:
+            return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
     }
 }
