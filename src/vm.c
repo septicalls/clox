116a3084a8503a2d081594a019cd04cc6af5425f
septicalls <septicalls@proton.me>
2023-09-06 14:39:40 +0530

    added debugging for stacks

--- src/vm.c
+++ src/vm.c
@@ -34,17 +34,25 @@ static InterpretResult run() {
 
     for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION
+        printf("        ");
+        for (Value *slot; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
     #endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                printValue(constant);
-                printf("\n");
+                push(constant);
                 break;
             }
             case OP_RETURN: {
+                printValue(pop());
+                printf("\n");
                 return INTERPRET_OK;
             }
         }
