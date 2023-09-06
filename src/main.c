dd7f73bfcd9f73c0a54e935fb212b8dd2cce5faa
septicalls <septicalls@proton.me>
2023-09-06 12:24:52 +0530

    wired vm to the interpreter

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char *argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
@@ -13,6 +16,8 @@ int main(int argc, char *argv[]) {
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
