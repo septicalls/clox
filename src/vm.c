dc0ba3a5f2fa6a767bd58ec0026eee37e44372c4
septicalls <septicalls@proton.me>
2023-09-09 14:51:20 +0530

    made binary arithmetix operations work

--- src/vm.c
+++ src/vm.c
@@ -31,18 +31,24 @@ Value pop() {
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(op) \
+        do {\
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while (false)
 
     for (;;) {
-    #ifdef DEBUG_TRACE_EXECUTION
-        printf("        ");
-        for (Value *slot; slot < vm.stackTop; slot++) {
-            printf("[ ");
-            printValue(*slot);
-            printf(" ]");
-        }
-        printf("\n");
-        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
-    #endif
+#ifdef DEBUG_TRACE_EXECUTION
+    printf("        ");
+    for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
+        printf("[ ");
+        printValue(*slot);
+        printf(" ]");
+    }
+    printf("\n");
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
         uint8_t instruction;
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
@@ -50,6 +56,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:        BINARY_OP(+); break;
+            case OP_SUBTRACT:   BINARY_OP(-); break;
+            case OP_MULTIPLY:   BINARY_OP(*); break;
+            case OP_DIVIDE:     BINARY_OP(/); break;
+            case OP_NEGATE:     push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -57,10 +68,11 @@ static InterpretResult run() {
             }
         }
     }
-    
-    #undef READ_BYTE()
-    #undef READ_CONSTANT()
+    #undef READ_BYTE
+    #undef READ_CONSTANT
+    #undef BINARY_OP
 }
+
 InterpretResult interpret(Chunk *chunk) {
     vm.chunk = chunk;
     vm.ip = vm.chunk->code;
