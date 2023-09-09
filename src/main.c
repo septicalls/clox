55a9e55be0f4a668afa4bfb4682e41c2fc57275f
septicalls <septicalls@proton.me>
2023-09-09 22:50:42 +0530

    hand-compiled 1 + 2 * 3 - 4 / -5

--- src/main.c
+++ src/main.c
@@ -9,26 +9,46 @@ int main(int argc, char *argv[]) {
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    // Load 2 onto the stack
+    int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 5);
+    writeChunk(&chunk, constant, 5);
 
-    constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    // Load 3 onto the stack
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 4);
+    writeChunk(&chunk, constant, 4);
 
-    writeChunk(&chunk, OP_ADD, 123);
+    // Put 2 * 3 onto the stack
+    writeChunk(&chunk, OP_MULTIPLY, 6);
+    // Load 4 onto the stack
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 2);
+    writeChunk(&chunk, constant, 2);
 
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    // Load -5 onto the stack
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, constant, 1);
+    writeChunk(&chunk, OP_NEGATE, 1);
 
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
+    // Put 4 / -5 on the stack
+    writeChunk(&chunk, OP_DIVIDE, 3);
 
-    writeChunk(&chunk, OP_RETURN, 123);
+    // Put (2 * 3) - (4 / -5) on the stack
+    writeChunk(&chunk, OP_SUBTRACT, 7);
 
-    disassembleChunk(&chunk, "test chunk");
+    // Load 1 onto the stack
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 8);
+    writeChunk(&chunk, constant, 8);
+
+    // Put 1 + ((2 * 3) - (4 / -5)) onto the stack
+    writeChunk(&chunk, OP_ADD, 9);
+
+    // Return!
+    writeChunk(&chunk, OP_RETURN, 10);
+
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
