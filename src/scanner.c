6ec0ba3600483db49cfd7c9195e9614e8c3f9a94
septicalls <septicalls@proton.me>
2023-09-14 00:11:15 +0530

    scanned identifiers and keywords

--- src/scanner.c
+++ src/scanner.c
@@ -18,6 +18,12 @@ void initScanner(const char *source) {
     scanner.line = 1;
 }
 
+static bool isAlpha(const char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+}
+
 static bool isDigit(const char c) {
     return c >= '0' && c <= '9';
 }
@@ -89,6 +95,47 @@ static void skipWhitespace() {
     }
 }
 
+static TokenType checkKeyword(int start, int length
+    const char *rest, TokenType type) {
+    if (scanner.current - scanner.start == start + length &&
+        memcmp(scanner.start + start, rest, length) == 0) {
+            return type;
+        }
+
+    return TOKEN_IDENTIFIER;
+}
+
+static TokenType identifierType() {
+    switch(scanner.start[0]) {
+        case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e': return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f':
+            if (scanner.current - scanner.start > 1) {
+                switch(scanner.start[1]) {
+                    case 'a': return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o': return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u': return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        case 'i': return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n': return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o': return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p': return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r': return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's': return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 'v': return checkKeyword(1, 2, "ar", TOKEN_VAR);
+        case 'w': return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+    }
+    return TOKEN_IDENTIFIER;
+}
+
+static Token identifier() {
+    while(isAlpha(peek()) || isDigit(peek())) advance();
+    return makeToken(identifierType());
+}
+
 static Token number() {
     while(isDigit(peek())) advance();
 
@@ -121,6 +168,7 @@ Token scanToken() {
     scanner.start = scanner.current;
 
     char c = advance();
+    if (isAlpha(c)) return identifier();
     if (isDigit(c)) return number();
 
     switch(c) {
