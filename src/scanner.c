2f9244890629522381737a0d175d27ac1f2d4982
septicalls <septicalls@proton.me>
2023-09-13 22:59:36 +0530

    scanned all punctuation-like tokens

--- src/scanner.c
+++ src/scanner.c
@@ -22,6 +22,18 @@ static bool isAtEnd() {
     return *scanner.current == '\0';
 }
 
+static char advance() {
+    scanner.current++;
+    return scanner.current[-1];
+}
+
+static bool match(const char expected) {
+    if (isAtEnd()) return false;
+    if (*scanner.current != expected) return false;
+    scanner.current++;
+    return true;
+}
+
 static Token makeToken(TokenType type) {
     Token token;
     token.type = type;
@@ -41,6 +53,30 @@ static Token errorToken(const char *message) {
 
 Token scanToken() {
     scanner.start = scanner.current;
+
+    char c = advance();
+
+    switch(c) {
+        case '(': return makeToken(TOKEN_LEFT_PAREN);
+        case ')': return makeToken(TOKEN_RIGHT_PAREN);
+        case '{': return makeToken(TOKEN_LEFT_BRACE);
+        case '}': return makeToken(TOKEN_RIGHT_BRACE);
+        case ';': return makeToken(TOKEN_SEMICOLON);
+        case ',': return makeToken(TOKEN_COMMA);
+        case '.': return makeToken(TOKEN_DOT);
+        case '-': return makeToken(TOKEN_MINUS);
+        case '+': return makeToken(TOKEN_PLUS);
+        case '/': return makeToken(TOKEN_SLASH);
+        case '*': return makeToken(TOKEN_STAR);
+        case '!': return makeToken(
+            match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+        case '=': return makeToken(
+            match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+        case '<': return makeToken(
+            match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+        case '>': return makeToken(
+            match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
 
     if (isAtEnd()) return makeToken(TOKEN_EOF);
 
