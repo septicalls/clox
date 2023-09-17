83ebf52ad6547bc80d477db827d01245575d8a90
septicalls <septicalls@proton.me>
2023-09-17 11:54:42 +0530

    updated compiler definition

--- src/compiler.c
+++ src/compiler.c
@@ -4,19 +4,9 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source) {
+void compile(const char *source, Chunk *chunk) {
     initScanner(source);
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start); 
-
-        if (token.type == TOKEN_EOF) break;
-    }
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
