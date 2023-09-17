5a49634dfdcebb9fe1c096d73103c9e31cbd80f7
septicalls <septicalls@proton.me>
2023-09-17 12:20:17 +0530

    parsed tokens and handled errors

--- src/compiler.c
+++ src/compiler.c
@@ -1,12 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source, Chunk *chunk) {
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token *token, const char *message) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // No-op
+    } else {
+        fprintf(stderr, "at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char *message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char *message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+    
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char *message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+bool compile(const char *source, Chunk *chunk) {
     initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    return !parser.hadError;
 }
