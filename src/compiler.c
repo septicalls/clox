0efbe53a5a281bc0e14da9bce1461efadd42817b
septicalls <septicalls@proton.me>
2023-09-17 19:10:48 +0530

    setup debugging for generated bytecode

--- src/compiler.c
+++ src/compiler.c
@@ -5,6 +5,10 @@
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
 typedef struct {
     Token current;
     Token previous;
@@ -115,6 +119,11 @@ static void emitConstant(Value value) {
 
 static void endCompiler() {
     emitReturn();
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError) {
+        disassembleChunk(currentChunk(), "code");
+    }
+#endif
 }
 
 static void expression();
