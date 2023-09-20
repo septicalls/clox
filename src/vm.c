beea3633cd580f11af72e4f510f2434b87b60f90
septicalls <septicalls@proton.me>
2023-09-20 14:17:39 +0530

    added runtime error handling for Values

--- src/vm.c
+++ src/vm.c
@@ -12,6 +12,19 @@ static void resetStack() {
     vm.stackTop = vm.stack;
 }
 
+static void runtimeError(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
+}
+
 void initVM() {
     resetStack();
 }
@@ -39,11 +52,10 @@ static InterpretResult run() {
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
     #define BINARY_OP(valueType, op) \
         do { \
-    #define BINARY_OP(op) \
-        do {\
-            double b = pop(); \
-            double a = pop(); \
-            push(a op b); \
+            if (!IS_NUMBER(peek(0)) && !IS_NUMBER(peek(1))) { \
+                runtimeError("Operands must be numbers."); \
+                return INTERPRET_RUNTIME_ERROR; \
+            } \
             double b = AS_NUMBER(pop()); \
             double a = AS_NUMBER(pop()); \
             push(valueType(a op b)); \
@@ -72,11 +84,10 @@ static InterpretResult run() {
             case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
             case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
             case OP_NEGATE:
-            case OP_ADD:        BINARY_OP(+); break;
-            case OP_SUBTRACT:   BINARY_OP(-); break;
-            case OP_MULTIPLY:   BINARY_OP(*); break;
-            case OP_DIVIDE:     BINARY_OP(/); break;
-            case OP_NEGATE:     push(-pop()); break;
+                if (!IS_NUMBER(peek(0))) {
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
                 push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
             case OP_RETURN: {
