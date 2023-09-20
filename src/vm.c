a23d30d01e8669d86010862107ad4dc824d41eef
septicalls <septicalls@proton.me>
2023-09-20 14:17:04 +0530

    updated code to use Value macros

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -29,14 +30,23 @@ Value pop() {
     return *vm.stackTop;
 }
 
+static Value peek(int distance) {
+    return vm.stackTop[-1 - distance];
+}
+
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(valueType, op) \
+        do { \
     #define BINARY_OP(op) \
         do {\
             double b = pop(); \
             double a = pop(); \
             push(a op b); \
+            double b = AS_NUMBER(pop()); \
+            double a = AS_NUMBER(pop()); \
+            push(valueType(a op b)); \
         } while (false)
 
     for (;;) {
@@ -57,11 +67,18 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:     BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NEGATE:
             case OP_ADD:        BINARY_OP(+); break;
             case OP_SUBTRACT:   BINARY_OP(-); break;
             case OP_MULTIPLY:   BINARY_OP(*); break;
             case OP_DIVIDE:     BINARY_OP(/); break;
             case OP_NEGATE:     push(-pop()); break;
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
+                break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
