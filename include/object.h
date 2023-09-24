28b7f9b3f023ecd0df40d7af7742ae0ec526869c
septicalls <septicalls@proton.me>
2023-09-24 16:58:54 +0530

    added support for heap allocated obj types

---
+++ include/object.h
@@ -0,0 +1,16 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+#define OBJ_TYPE(value)         (AS_OBJ(value)->type)
+typedef enum {
+    OBJ_STRING
+} ObjType;
+
+struct Obj {
+    ObjType type;
+};
+
+#endif
