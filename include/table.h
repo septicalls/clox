75a3cde21202ae95fd12de6f7993fe29559fcf72
septicalls <septicalls@proton.me>
2023-10-13 20:26:22 +0530

    defined a hash table

---
+++ include/table.h
@@ -0,0 +1,21 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "common.h"
+#include "value.h"
+
+typedef struct {
+    ObjString *key;
+    Value value;
+} Entry;
+
+typedef struct {
+    int count;
+    int capacity;
+    Entry* entries;
+} Table;
+
+void initTable(Table *table);
+void freeTable(Table *table);
+
+#endif
---
+++ src/table.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "table.h"
+#include "value.h"
+
+void initTable(Table *table) {
+    table->count = 0;
+    table->capacity = 0;
+    table->entries = NULL;
+}
+
+void freeTable(Table *table) {
+    FREE_ARRAY(Entry, table->entries, table->capacity);
+    initTable(table);
+}
