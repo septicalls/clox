93741cebb954f782ec12d7aec622dfe106cce1bd
septicalls <septicalls@proton.me>
2023-10-13 21:13:02 +0530

    defined functions for retrieving entries

--- include/table.h
+++ include/table.h
@@ -17,6 +17,7 @@ typedef struct {
 
 void initTable(Table *table);
 void freeTable(Table *table);
+bool tableGet(Table *table, ObjString *key, Value *value);
 bool tableSet(Table *table, ObjString *key, Value value);
 void tableAddAll(Table *from, Table *to);
 
--- src/table.c
+++ src/table.c
@@ -31,6 +31,16 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
     }
 }
 
+bool tableGet(Table *table, ObjString *key, Value *value) {
+    if (table->count == 0) return false;
+
+    Entry *entry = findEntry(table->entries, table->capacity, key);
+    if (entry->key == NULL) return false;
+
+    *value = entry->value;
+    return true;
+}
+
 static void adjustCapacity(Table *table, int capacity) {
     Entry *entries = ALLOCATE(Entry, capacity);
     for (int i = 0; i < capacity; i++) {
