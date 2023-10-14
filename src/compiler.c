34aef3190fe6784cbdd13683a7aea635a1b94836
septicalls <septicalls@proton.me>
2023-10-14 15:33:18 +0530

    added support for expression statements

--- src/compiler.c
+++ src/compiler.c
@@ -268,6 +268,11 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+    emitByte(OP_POP);
+}
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -281,6 +286,8 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else {
+        expressionStatement();
     }
 }
 
