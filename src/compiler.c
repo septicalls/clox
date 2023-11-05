f78a25b14b05f07e861550b9134cd399c771f153
septicalls <septicalls@proton.me>
2023-11-05 17:25:47 +0530

    tended to a variable self-reference edge-case

--- src/compiler.c
+++ src/compiler.c
@@ -188,6 +188,9 @@ static int resolveLocal(Compiler *compiler, Token *name) {
     for (int i = compiler->localCount - 1; i >= 0; i--) {
         Local *local = &compiler->locals[i];
         if (identifiersEqual(name, &local->name)) {
+            if (local->depth == -1) {
+                error("Can't read local variable in its own initialiser.");
+            }
             return i;
         }
     }
@@ -203,7 +206,7 @@ static void addLocal(Token name) {
 
     Local *local = &current->locals[current->localCount++];
     local->name = name;
-    local->depth = current->scopeDepth;
+    local->depth = -1;
 }
 
 static void declareVariable() {
@@ -380,8 +383,13 @@ static uint8_t parseVariable(const char *errorMessage) {
     return identifierConstant(&parser.previous);
 }
 
+static void markInitialised() {
+    current->locals[current->localCount - 1].depth = current->scopeDepth;
+}
+
 static void defineVariable(uint8_t global) {
     if (current->scopeDepth > 0) {
+        markInitialised();
         return;
     }
 
