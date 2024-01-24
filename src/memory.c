f38084821b34d8dcc9b528446257bbec372cbd58
septicalls <septicalls@proton.me>
2024-01-24 23:09:16 +0530

    processed gray objects

--- src/memory.c
+++ src/memory.c
@@ -28,6 +28,8 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 
 void markObject(Obj *object) {
     if (object == NULL) return;
+    if (object->isMarked) return;
+
 #ifdef DEBUG_LOG_GC
     printf("%p mark ", (void *)object);
     printValue(OBJ_VAL(object));
@@ -48,6 +50,43 @@ void markObject(Obj *object) {
 
 void markValue(Value value) {
     if (IS_OBJ(value)) markObject(AS_OBJ(value));
+}
+
+static void markArray(ValueArray *array) {
+    for (int i = 0; i < array->count; i++) {
+        markValue(array->values[i]);
+    }
+}
+
+static void blackenObject(Obj *object) {
+
+#ifdef DEBUG_LOG_GC
+    printf("%p blacken ", (void *)object);
+    printValue(OBJ_VAL(object));
+    printf("\n");
+#endif
+
+    switch (object->type) {
+        case OBJ_CLOSURE: {
+            ObjClosure *closure = (ObjClosure *)object;
+            markObject((Obj *)closure->function);
+            for (int i = 0; i < closure->upvalueCount; i++) {
+                markObject((Obj *)closure->upvalues[i]);
+            }
+            break;
+        }
+        case OBJ_FUNCTION:
+            ObjFunction *function = (ObjFunction *)object;
+            markObject((Obj *)function->name);
+            markArray(&function->chunk.constants);
+            break;
+        case OBJ_UPVALUE:
+            markValue(((ObjUpvalue *)object)->closed);
+            break;
+        case OBJ_NATIVE:
+        case OBJ_STRING:
+            break;
+    }
 }
 
 static void freeObject(Obj *object) {
@@ -104,12 +143,20 @@ static void markRoots() {
     markCompilerRoots();
 }
 
+static void traceReferences() {
+    while (vm.grayCount > 0) {
+        Obj *object = vm.grayStack[--vm.grayCount];
+        blackenObject(object);
+    }
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
 #endif
 
     markRoots();
+    traceReferences();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc ends\n");
