a4d91842e50edb532e17ac4fd373c1b43e63d59a
septicalls <septicalls@proton.me>
2024-01-26 21:41:30 +0530

    set up calling the GC

--- include/vm.h
+++ include/vm.h
@@ -22,6 +22,9 @@ typedef struct {
     Table globals;
     Table strings;
     ObjUpvalue *openUpvalues;
+
+    size_t bytesAllocated;
+    size_t nextGC;
     Obj *objects;
     int grayCount;
     int grayCapacity;
--- src/memory.c
+++ src/memory.c
@@ -10,11 +10,18 @@
 #include "debug.h"
 #endif
 
+#define GC_HEAP_GROW_FACTOR 2
+
 void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
+    vm.bytesAllocated += newSize - oldSize;
     if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
         collectGarbage();
 #endif
+    }
+
+    if (vm.bytesAllocated > vm.nextGC) {
+        collectGarbage();
     }
 
     if (newSize == 0) {
@@ -176,6 +183,7 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
+  size_t before = vm.bytesAllocated;
 #endif
 
     markRoots();
@@ -183,8 +191,12 @@ void collectGarbage() {
     tableRemoveWhite(&vm.strings);
     sweep();
 
+    vm.nextGC = vm.bytesAllocated * GC_HEAP_GROW_FACTOR;
+
 #ifdef DEBUG_LOG_GC
   printf("-- gc ends\n");
+  printf("   collected %zu bytes (from %zu to %zu) next at %zu\n",
+         before - vm.bytesAllocated, before, vm.bytesAllocated, vm.nextGC);
 #endif
 }
 
