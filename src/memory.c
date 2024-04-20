bad4d75b3e33e8678d1bb3c9c9440e533943a839
septicalls <septicalls@proton.me>
2024-04-20 20:31:24 +0530

    defined run-time representation of a class

--- src/memory.c
+++ src/memory.c
@@ -75,6 +75,11 @@ static void blackenObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_CLASS: {
+            ObjClass *klass = (ObjClass *)object;
+            markObject((Obj *)klass->name);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *)object;
             markObject((Obj *)closure->function);
@@ -103,6 +108,10 @@ static void freeObject(Obj *object) {
 #endif
 
     switch (object->type) {
+        case OBJ_CLASS: {
+            FREE(ObjClass, object);
+            break;
+        }
         case OBJ_CLOSURE: {
             ObjClosure *closure = (ObjClosure *)object;
             FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
