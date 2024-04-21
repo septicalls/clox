533aa19f6a0aa93058f97e937059c405fab4b509
septicalls <septicalls@proton.me>
2024-04-21 17:45:27 +0530

    represented methods in runtime

--- include/object.h
+++ include/object.h
@@ -8,6 +8,7 @@
 
 #define OBJ_TYPE(value)         (AS_OBJ(value)->type)
 
+#define IS_BOUND_METHOD(value)  isObjType(value, OBJ_BOUND_METHOD)
 #define IS_CLASS(value)         isObjType(value, OBJ_CLASS)
 #define IS_CLOSURE(value)       isObjType(value, OBJ_CLOSURE)
 #define IS_FUNCTION(value)      isObjType(value, OBJ_FUNCTION)
@@ -16,6 +17,7 @@
 #define IS_STRING(value)        isObjType(value, OBJ_STRING)
 
 #define AS_CLASS(value)         ((ObjClass *)AS_OBJ(value))
+#define AS_BOUND_METHOD(value)  ((ObjBoundMethod *)AS_OBJ(value))
 #define AS_CLOSURE(value)       ((ObjClosure *)AS_OBJ(value))
 #define AS_FUNCTION(value)      ((ObjFunction *)AS_OBJ(value))
 #define AS_INSTANCE(value)      ((ObjInstance *)AS_OBJ(value))
@@ -25,6 +27,7 @@
 #define AS_CSTRING(value)       (((ObjString *)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_BOUND_METHOD,
     OBJ_CLASS,
     OBJ_CLOSURE,
     OBJ_FUNCTION,
@@ -88,6 +91,13 @@ typedef struct {
     Table fields;
 } ObjInstance;
 
+typedef struct {
+    Obj obj;
+    Value receiver;
+    ObjClosure *method;
+} ObjBoundMethod;
+
+ObjBoundMethod *newBoundMethod(Value receiver, ObjClosure *method);
 ObjClass *newClass(ObjString *name);
 ObjClosure *newClosure(ObjFunction *function);
 ObjFunction *newFunction();
