9f4daec3fa398f953af95ff05c555a9045128fe5
septicalls <septicalls@proton.me>
2024-04-22 23:00:07 +0530

    added support for method inheritance

--- src/compiler.c
+++ src/compiler.c
@@ -661,6 +661,18 @@ static void classDeclaration() {
     classCompiler.enclosing = currentClass;
     currentClass = &classCompiler;
 
+    if (match(TOKEN_LESS)) {
+        consume(TOKEN_IDENTIFIER, "Expect superclass name.");
+        variable(false);
+
+        if (identifiersEqual(&className, &parser.previous)) {
+            error("A class can't inherit from itself.");
+        }
+
+        namedVariable(className, false);
+        emitByte(OP_INHERIT);
+    }
+
     namedVariable(className, false);
     consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
     while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
